3382311d423db292ab71cbcb74222dfefff0e7a5
Orixandro <orixandro@gmail.com>
2025-08-05 16:58:26 +0200

    Complete exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let leters = string.split("")
+    let reversed = ""
+    for (leter of leters) {
+        reversed = leter + reversed
+    }
+    return reversed
 };
 
 // Do not edit below this line
