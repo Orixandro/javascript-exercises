aad21c9da28544f16f4fcd0f1d99fed84662ffd8
Orixandro <orixandro@gmail.com>
2025-08-10 16:21:13 +0200

    Complete exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (typeof num === "string") {
+        num = +num
+    }
+    if (num < 0) {
+        return "OOPS"
+    }
+    let fibNum = 0
+    let prev = 1
+    let prev2 = 0
+    for (let i = 1; i <= num; i++) {
+        prev2 = prev
+        prev = fibNum
+        fibNum = prev + prev2
+    }
+    return fibNum
 };
 
 // Do not edit below this line
