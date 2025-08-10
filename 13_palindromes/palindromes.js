04790e0e0f7a67cf56fadd6df5df9b04fe64b74e
Orixandro <orixandro@gmail.com>
2025-08-10 16:09:22 +0200

    Solve exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let punctuationLess = string.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()@\+\?><\[\]\+]/g, '')
+    let array = punctuationLess.split("")
+    let simpleArray = array.filter((leter) => (leter !== " "))
+    return (simpleArray.toString().toLowerCase() === simpleArray.reverse().toString().toLowerCase())
 };
 
 // Do not edit below this line
