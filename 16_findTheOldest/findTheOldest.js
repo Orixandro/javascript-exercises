12ec8bd3a8f547c3c91342a58e985a7264e5e4d6
Orixandro <orixandro@gmail.com>
2025-08-10 16:38:37 +0200

    Complete exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,33 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+    return peopleArray.reduce((oldest, person) => {
+        let death
 
+        if (person.yearOfDeath === undefined) {
+            let today = new Date
+            death = today.getFullYear()
+        } else {
+            death = person.yearOfDeath
+        }
+
+        let oldestDeath
+
+        if (oldest.yearOfDeath === undefined) {
+            let today = new Date
+            oldestDeath = today.getFullYear()
+        } else {
+            oldestDeath = oldest.yearOfDeath
+        }
+
+        let age = death - person.yearOfBirth
+
+        let oldestAge = oldestDeath - oldest.yearOfBirth
+
+        if (age > oldestAge) {
+            return person
+        } else {
+            return oldest
+        }
+    })
 };
 
 // Do not edit below this line
